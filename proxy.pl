92ee9fb7caf95174058902a1fc942c82918364db
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-01-04 14:23:28 +0100

    implement man-in-the-middle ssl proxy

--- proxy.pl
+++ proxy.pl
@@ -11,6 +11,76 @@ use HTTP::Proxy::HeaderFilter::simple;
 
 use Data::Dump qw(dump);
 
+#
+# handle HTTPS CONNECT using main-in-the-middle SSL attack
+# from http://cpansearch.perl.org/src/BOBTFISH/HTTP-Proxy-Selective-0.004/script/selective_proxy
+#
+
+use IO::Socket::SSL;
+
+sub _handle_CONNECT {
+    my ($self, $served) = @_;
+warn "XXX";
+    my $last = 0;
+    my $conn = $self->client_socket;
+    my $req  = $self->request;
+    my $upstream = IO::Socket::INET->new( PeerAddr => $req->uri->host_port );
+    unless( $upstream and $upstream->connected ) {
+        # 502 Bad Gateway / 504 Gateway Timeout
+        # Note to implementors: some deployed proxies are known to
+        # return 400 or 500 when DNS lookups time out.
+        my $response = HTTP::Response->new( 200 );
+        $response->content_type( "text/plain" );
+        $self->response($response);
+        return $last;
+    }
+
+    # send the response headers (FIXME more headers required?)
+    my $response = HTTP::Response->new(200);
+    $self->response($response);
+    $self->{$_}{response}->select_filters( $response ) for qw( headers body );
+
+    $self->_send_response_headers( $served );
+
+    # we now have a TCP connection to the upstream host
+    $last = 1;
+    my $class = ref($conn);
+    { no strict 'refs'; unshift(@{$class . "::ISA"}, 'IO::Socket::SSL'); } # Forcibly change classes the socket inherits from
+    $class->start_SSL($conn, 
+        SSL_server => 1, 
+        SSL_key_file => './key',
+        SSL_cert_file => './cert', # Turn our client socket into SSL.
+    ) or warn("Could not start SSL");
+    ${*$conn}{'httpd_nomore'} = 0; # Pay no attention to the Connection: close header behind the curtain.
+    {   # Build a method to fiddle with the request object we get from the client, as it needs to http->https
+        my $old_setrequest_method = \&HTTP::Proxy::request;
+        my $new_request_method = sub {
+            my ($self, $new_req) = @_;
+            if ($new_req) {
+                use Data::Dumper;
+                if (!$new_req->uri->scheme or $new_req->uri->scheme eq 'http') {
+                    $new_req->uri->scheme('https');
+                    $new_req->uri->host($new_req->header('Host'));
+                }
+            }
+            $old_setrequest_method->($self, $new_req);
+        };
+        # And monkeypatch it into HTTP proxy, using local to restrict it by lexical scope
+        # so that it goes away once we exit the block (i.e. the CONNECT method finishes).
+        no warnings qw[once redefine];
+        local *HTTP::Proxy::request = $new_request_method;
+        use warnings qw[once redefine];
+        $self->serve_connections($conn);
+    }
+    $conn->stop_SSL($conn);
+    return $last;
+}
+{
+    no warnings qw(once redefine);
+    *HTTP::Proxy::_handle_CONNECT = \&_handle_CONNECT;
+}
+
+
 sub var_save {
 	my ( $dir, $name, $value ) = @_;
 	$value ||= "\n";
@@ -210,7 +280,7 @@ $proxy->push_filter(
             }
         }
     )
-);
+) if 0; # FIXME remote for production!
 
 
 #
